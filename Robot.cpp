d642ab72cbd472d69b1d694e51077cdf0a54d7e2
YassineOurara <yassineourara@gmail.com>
2022-11-18 21:04:53 +0100

    Update Robot.cpp
    update

--- Robot.cpp
+++ Robot.cpp
@@ -3,32 +3,44 @@
   int main() 
   
   {
-  	initwindow(600,600); 
+  	initwindow(800,800); 
     moveto(0,0);          
     setcolor(15);
- 	int x = 85, y = 85;
+ 	int x = 90, y = 90, r=80;
     while (1) 
 	{
-    circle(x,y,50);
+	//////////Dessin Robot///////////
+	// Cercle
+    circle(x,y,r);
+    // Triangle
+    line(x-r, y, x+r, y);
+    line(x-r, y, x, y-r);
+    line(x+r, y, x,y-r);
+	//Roues
+	line(x-r/4, y-r/4, x-r/4, y+r/4);
+    line(x+r/4, y-r/4, x+r/4,y+r/4);
+	
+    
+
             if (GetAsyncKeyState(VK_RIGHT)) {
             	cleardevice();
-                x += 10;
-                delay(1);
+                x += 1;
+                delay(10);
                 
             }
             else if (GetAsyncKeyState(VK_LEFT)) {
             	cleardevice();
-                x -= 10;
-                delay(1);
+                x -= 1;
+                delay(10);
                 
             }else if (GetAsyncKeyState(VK_UP)) {
                 cleardevice();
-                y -= 10;
-                delay(1);
+                y -= 1;
+                delay(10);
             }else if (GetAsyncKeyState(VK_DOWN)) {
                 cleardevice();
-                y += 10;
-                delay(1);
+                y += 1;
+                delay(10);
             }
             if (GetAsyncKeyState(VK_RETURN)) {
                 break;
