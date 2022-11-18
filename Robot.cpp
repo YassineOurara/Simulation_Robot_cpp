f243061d6493f853616039c4e4a857cf3f6ae935
YassineOurara <yassineourara@gmail.com>
2022-11-18 23:10:08 +0100

    .....
    .....

--- Robot.cpp
+++ Robot.cpp
@@ -5,23 +5,30 @@
   {
   	initwindow(800,800); 
     moveto(0,0);          
-    setcolor(15);
+    setcolor(14);
+    //Variables
  	int x = 90, y = 90, r=80;
+ 	
+	
     while (1) 
 	{
-	//////////Dessin Robot///////////
+			//////////Dessin Robot///////////
 	// Cercle
     circle(x,y,r);
     // Triangle
     line(x-r, y, x+r, y);
     line(x-r, y, x, y-r);
     line(x+r, y, x,y-r);
-	//Roues
+	//Roue 1
 	line(x-r/4, y-r/4, x-r/4, y+r/4);
+    line(x-r/3, y-r/4, x-r/3, y+r/4);
+    line(x-r/4, y-r/4, x-r/3, y-r/4);
+    line(x-r/4, y+r/4, x-r/3, y+r/4);
+    //Roue 2
     line(x+r/4, y-r/4, x+r/4,y+r/4);
-	
-    
-
+    line(x+r/3, y-r/4, x+r/3,y+r/4);
+    line(x+r/4, y-r/4, x+r/3, y-r/4);
+    line(x+r/4, y+r/4, x+r/3, y+r/4);
             if (GetAsyncKeyState(VK_RIGHT)) {
             	cleardevice();
                 x += 1;
@@ -42,6 +49,20 @@
                 y += 1;
                 delay(10);
             }
+             else if (GetAsyncKeyState(VK_LBUTTON)) {
+            	cleardevice();
+                x += 1;
+                y += 1;
+                delay(10);
+                
+            }
+             else if (GetAsyncKeyState(VK_RBUTTON)) {
+            	cleardevice();
+                x -= 1;
+                y -= 1;
+                delay(10);
+                
+            }
             if (GetAsyncKeyState(VK_RETURN)) {
                 break;
                 delay(10);
@@ -49,5 +70,5 @@
     }
 	closegraph(); 
              
-      return 0; 
+    return 0; 
   }
