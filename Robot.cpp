a7eee2c60e340b7f58e69461f73d46b5c28047cd
YassineOurara <yassineourara@gmail.com>
2022-11-18 14:32:00 +0100

    Test....

---
+++ Robot.cpp
@@ -0,0 +1,41 @@
+ #include <graphics.h>
+
+  int main() 
+  
+  {
+  	initwindow(600,600); 
+    moveto(0,0);          
+    setcolor(15);
+ 	int x = 100, y = 100;
+    while (1) 
+	{
+    circle(x,y,50);
+            if (GetAsyncKeyState(VK_RIGHT)) {
+            	cleardevice();
+                x += 10;
+                delay(1);
+                
+            }
+            else if (GetAsyncKeyState(VK_LEFT)) {
+            	cleardevice();
+                x -= 10;
+                delay(1);
+                
+            }else if (GetAsyncKeyState(VK_UP)) {
+                cleardevice();
+                y -= 10;
+                delay(1);
+            }else if (GetAsyncKeyState(VK_DOWN)) {
+                cleardevice();
+                y += 10;
+                delay(1);
+            }
+            if (GetAsyncKeyState(VK_RETURN)) {
+                break;
+                delay(10);
+            }
+    }
+	closegraph(); 
+             
+      return 0; 
+  }
