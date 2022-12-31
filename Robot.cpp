d063d28080c569cf3995320c9ff6c892bdcf2ee7
YassineOurara <yassineourara@gmail.com>
2022-12-31 10:55:05 +0100

    up

--- Robot.cpp
+++ Robot.cpp
@@ -4,12 +4,17 @@
   int main() 
   
   {
-  	initwindow(800,800); 
+  	DWORD w=GetSystemMetrics(SM_CXSCREEN);
+    DWORD h=GetSystemMetrics(SM_CYSCREEN);
+    h=1100;
+    w=1500;
+  	initwindow(w,h,"Mini_Projet Simulation"); 
     moveto(0,0);          
     setcolor(15);
     //Variables
- 	int x = 400, y = 700, r=80;
- 	
+ 	int x = 400, y = 700, r=50;
+	float theta=30;
+	float alpha,beta;
     while (1) 
 	{
 			//////////Dessin Robot///////////
@@ -73,6 +78,21 @@
             }
             else if (GetAsyncKeyState(VK_SPACE)) {
             	cleardevice();
+            	alpha=theta*3.14f/180;
+            	beta=(theta+90)*3.14f/180;
+            	//////////////////
+//            	x=x*cos(alpha);
+//				y=y*sin(beta);
+//			    line(x-r, y, x*sin(alpha), y-r*cos(alpha));
+//			    line(x+r, y, x*sin(beta),y-r*cos(beta));
+
+				////////////////////
+//				line(x,y,x+r*sin(alpha),y+r*cos(alpha));
+//				line(x,y,x+r*sin(beta),y+r*cos(beta));
+//				line(x,y,x-r*sin(alpha),y-r*cos(alpha));
+//				line(x,y,x-r*sin(beta),y-r*cos(beta));
+				theta-=8;
+				delay(50);
             }
             if (GetAsyncKeyState(VK_RETURN)) {
                 break;
@@ -80,6 +100,37 @@
             }
     }
 	closegraph(); 
+	
+	
+	int nbrObstacle=0;
+	dessin::dessin(){
+//Données
+// rayon robot
+    Rr=50;
+// rayon roue
+    R0=0.020;
+//intervall de temps entre chaque mise a jour de la position et de l'orientation du robot:
+    Dt=0.1;
+// distance entre les roues
+    D=0.05;
+// vitesse angulaire max des roues
+    w0Max=10;
+// Acceleration maximun des roues
+    Dw0Max=2;
+    alpha=1;
+    wg=0;
+    wd=0;
+// position de robot
+    Xr=60;
+    Yr=60;
+//position de but
+    Xc=1300;
+    Yc=900;
+// position des obstacles
+	ifstream file ("obstacles.obs");
+    int x_obs;
+    int y_obs;
+    int r_obs;
              
     return 0; 
   }
